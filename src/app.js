7c30b1422b5375eb60f9a5bccaee7eea8ffe085e
maahdi boudaouara <mboudaouara@mercarue.com>
2023-04-24 15:40:10 +0100

    458

--- src/app.js
+++ src/app.js
@@ -11,6 +11,11 @@ app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({
     extended: true
 }))
+const path = require("path");
+
+app.use(express.json())
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 
 //import routes
